ee8a8bb9fcb555ac815ed9e01558e3ddea4f54d1
satheeshk50 <146642346+satheeshk50@users.noreply.github.com>
2024-02-20 09:29:33 +0530

    Time: 1 ms (90.00%), Space: 41.7 MB (75.95%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,15 +1,27 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        String capital = word.toUpperCase();
-        if(capital.compareTo(word)==0) return true;
-        String small = word.toLowerCase();
-        if(small.compareTo(word)==0) return true;
-        // StringBuilder str = new StringBuilder(small);
-        for(int i=0;i<word.length();i++)
+        // String capital = word.toUpperCase();
+        // if(capital.compareTo(word)==0) return true;
+        // String small = word.toLowerCase();
+        // if(small.compareTo(word)==0) return true;
+        // // StringBuilder str = new StringBuilder(small);
+        // for(int i=0;i<word.length();i++)
+        // {
+        //     if(i==0&&(word.charAt(i)>='A'&&word.charAt(i)<='Z')) continue;
+        //     if(word.charAt(i)<'a'||word.charAt(i)>'z') return false;
+        // }
+        // return true;
+        int small=0;
+        int cap = 0;
+        int n = word.length();
+        for(int i=0;i<n;i++)
         {
-            if(i==0&&(word.charAt(i)>='A'&&word.charAt(i)<='Z')) continue;
-            if(word.charAt(i)<'a'||word.charAt(i)>'z') return false;
+            if(word.charAt(i)>='A'&&word.charAt(i)<='Z') cap++;
+            else small++;
         }
-        return true;
+        if(cap==n) return true;
+        if(small==n) return true;
+        if((word.charAt(0)>='A'&&word.charAt(0)<='Z')&&small==n-1) return true;
+        return false;
     }
 }
